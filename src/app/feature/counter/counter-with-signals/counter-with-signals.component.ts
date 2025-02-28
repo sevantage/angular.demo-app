242b5ef6db7eccf0a3f95a2b9a75ef9cdbef6c0c
M-Wildgruber <M-Wildgruber@users.noreply.github.com>
2025-02-28 16:22:17 +0100

    Added counter with signals

---
+++ src/app/feature/counter/counter-with-signals/counter-with-signals.component.ts
@@ -0,0 +1,30 @@
+import { Component, computed, model, output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-with-signals',
+  templateUrl: './counter-with-signals.component.html',
+  styleUrl: './counter-with-signals.component.scss',
+  standalone: false,
+})
+export class CounterWithSignalsComponent {
+  value = model(0);
+  // Zusätzliche Events
+  increased = output<number>();
+  decreased = output<number>();
+  // Computed value
+  computedValue = computed(() => this.value() * 2);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onIncrease() {
+    this.value.update((oldValue) => oldValue + 1);
+    this.increased.emit(this.value());
+  }
+
+  onDecrease() {
+    this.value.update((oldValue) => oldValue - 1);
+    this.decreased.emit(this.value());
+  }
+}
--- src/app/feature/counter/counter.module.ts
+++ src/app/feature/counter/counter.module.ts
@@ -1,24 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CounterComponent } from './counter/counter.component';
+import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CounterDemoComponent } from './counter-demo/counter-demo.component';
 import { CounterRoutingModule } from './counter-routing.module';
-
-
+import { CounterWithSignalsComponent } from './counter-with-signals/counter-with-signals.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     CounterComponent,
     CounterDemoComponent,
+    CounterWithSignalsComponent,
   ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    CounterRoutingModule,
-  ],
-  exports: [
-    CounterComponent
-  ]
+  imports: [CommonModule, SharedModule, CounterRoutingModule],
+  exports: [CounterComponent],
 })
-export class CounterModule { }
+export class CounterModule {}
