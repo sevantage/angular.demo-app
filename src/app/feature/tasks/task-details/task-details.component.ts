57f050c944ebdd43aa3ac030fc170758a0cbe634
M-Wildgruber <M-Wildgruber@users.noreply.github.com>
2025-03-03 10:59:11 +0100

    Used input binding for task-details

--- src/app/feature/tasks/task-details/task-details.component.ts
+++ src/app/feature/tasks/task-details/task-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Task } from 'src/app/core/tasks/task';
 import { TasksService } from 'src/app/core/tasks/tasks.service';
@@ -9,12 +9,17 @@ import { TasksService } from 'src/app/core/tasks/tasks.service';
   styleUrls: ['./task-details.component.scss'],
 })
 export class TaskDetailsComponent implements OnInit {
+  id = input.required<string>();
+
   task: Task = { id: '', description: '', isDone: false };
 
   constructor(private tasksSvc: TasksService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    const id: string = this.route.snapshot.params['id'];
-    this.tasksSvc.getTaskById(id).subscribe((task) => (this.task = task));
+    // Alternative that can be used instead of the input: Read id from Activated Route:
+    // const id: string = this.route.snapshot.params['id'];
+    this.tasksSvc
+      .getTaskById(this.id())
+      .subscribe((task) => (this.task = task));
   }
 }
